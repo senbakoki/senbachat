d6a8cf4a469cf4bdadcb3f0c60b74ff4956fece9
unknown <seiya1935963@gmail.com>
2021-06-04 20:44:11 +0900

    APIキー変えました

--- main.py
+++ main.py
@@ -1,71 +1,50 @@
-from __future__ import unicode_literals
-
+from flask import Flask,request,abort
+from linebot import LineBotApi,WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent,TextMessage,TextSendMessage
 import os
-import sys
-from argparse import ArgumentParser
+import requests
+import pprint
+import pya3rt
 
-from flask import Flask, request, abort
-from linebot import (
-    LineBotApi, WebhookParser
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
+app=Flask(__name__)
+#環境変数の取得
+YOUR_CHANNEL_ACCESS_TOKEN="PldzaAq46rJsbyql3Hh1NhE6keeeQ+fPFiWSFdWLVkywJg3oqJoF8Rbl1fidhn9iqdCdeb1LBsbe2F3OrifuJv/+lftDK3J9fI3P+b1y/qwfys7UtUpVu0SHYljehFHYg8TraTRswaX2pDgxozYE8gdB04t89/1O/w1cDnyilFU="
+YOUR_CHANNEL_SECRET="3a99190afb80db672eaa03e7613ad3ed"
+line_bot_api=LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler=WebhookHandler(YOUR_CHANNEL_SECRET)
 
-app = Flask(__name__)
-
-# get channel_secret and channel_access_token from your environment variable
-channel_secret = "3a99190afb80db672eaa03e7613ad3ed"
-channel_access_token = "PldzaAq46rJsbyql3Hh1NhE6keeeQ+fPFiWSFdWLVkywJg3oqJoF8Rbl1fidhn9iqdCdeb1LBsbe2F3OrifuJv/+lftDK3J9fI3P+b1y/qwfys7UtUpVu0SHYljehFHYg8TraTRswaX2pDgxozYE8gdB04t89/1O/w1cDnyilFU="
-if channel_secret is None:
-    print('Specify LINE_CHANNEL_SECRET as environment variable.')
-    sys.exit(1)
-if channel_access_token is None:
-    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
-    sys.exit(1)
-
-line_bot_api = LineBotApi(channel_access_token)
-parser = WebhookParser(channel_secret)
-
-
-@app.route("/callback", methods=['POST'])
+@app.route("/callback",methods=["POST"])
 def callback():
-    signature = request.headers['X-Line-Signature']
+    signature=request.headers["X-Line-Signature"]
 
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+    body=request.get_data(as_text=True)
+    app.logger.info("Request body"+body)
 
-    # parse webhook body
     try:
-        events = parser.parse(body, signature)
+        handler.handle(body,signature)
     except InvalidSignatureError:
         abort(400)
+    return "OK"
 
-    # if event is MessageEvent and message is TextMessage, then echo text
-    for event in events:
-        if not isinstance(event, MessageEvent):
-            continue
-        if not isinstance(event.message, TextMessage):
-            continue
+@handler.add(MessageEvent,message=TextMessage)
+def handle_message(event):
+    #入力された文字列を格納
+    push_text = event.message.text
 
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=event.message.text)
-        )
+    #A3RTのTalkAPIにより応答
+    reply_text = talkapi_response(push_text)
 
-    return 'OK'
+    #リプライ部分の記述
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=reply_text))
 
+#A3RTのTalkAPIにより応答
+def talkapi_response(text):
+    apikey = "DZZBCMj69WKPLY7IiJlCtb7DBx4UdmO1"
+    client = pya3rt.TalkClient(apikey)
+    response = client.talk(text)
+    return ((response['results'])[0])['reply']
 
-if __name__ == "__main__":
-    arg_parser = ArgumentParser(
-        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
-    )
-    arg_parser.add_argument('-p', '--port', type=int, default=8000, help='port')
-    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
-    options = arg_parser.parse_args()
-
-    app.run(debug=options.debug, port=options.port)
+if __name__=="__main__":
+    port=int(os.getenv("PORT",5000))
+    app.run(host="0.0.0.0",port=port)
